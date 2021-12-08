0a50f5114f1dd763325f7a5cfa7ff1a08bf4af12
POISON <86126808+Cat-of-Tg@users.noreply.github.com>
2021-12-08 23:04:22 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -54,7 +54,7 @@ HELP_STRINGS = """
 𝙰𝚗𝚍 𝚝𝚑𝚎 𝚏𝚘𝚕𝚕𝚘𝚠𝚒𝚗𝚐:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\n𝙰𝚕𝚕 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚌𝚊𝚗 𝚎𝚒𝚝𝚑𝚎𝚛 𝚋𝚎 𝚞𝚜𝚎𝚍 𝚠𝚒𝚝𝚑 / 𝚘𝚛 !.\n")
 
-TECHNO_IMG = "https://telegra.ph/file/10cf6a74f348caa0096b9.jpg"
+TECHNO_IMG = "https://telegra.ph/file/a81face9578539c9affb4.jpg"
 IMPORTED = {}
 MIGRATEABLE = []
 HELPABLE = {}
